d1a4d2a0fadc4d6425c2fc1a2945b9460229d5ef
Danieladequevedo <99833615+Danieladequevedo@users.noreply.github.com>
2024-05-09 19:37:03 -0400

    Modifica puerto

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ async def answer(pregunta: str):
     return {"Respuesta": respuesta}
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8080, host="0.0.0.0")
+    uvicorn.run(app, port=8000, host="0.0.0.0")
